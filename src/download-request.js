08027861c1f199f5cf17fcc4f40dbf22de35a036
Perry Mitchell <perry@perrymitchell.net>
2017-09-18 21:57:23 +0300

    Remove commented code

--- src/download-request.js
+++ src/download-request.js
@@ -5,7 +5,6 @@ var fetch = require('./fetch');
 
 var buildCustomError;
 var downloadRequest;
-// var nodeBinaryParser;
 
 // This doesn't match what was spec'd in paper doc yet
 buildCustomError = function (error) {
@@ -15,52 +14,12 @@ buildCustomError = function (error) {
   };
 };
 
-// nodeBinaryParser = function (res, done) {
-//   res.text = '';
-//   res.setEncoding('binary');
-//   res.on('data', function (chunk) { res.text += chunk; });
-//   res.on('end', function () {
-//     done();
-//   });
-// };
-
 downloadRequest = function (path, args, auth, host, accessToken, selectUser) {
   if (auth !== 'user') {
     throw new Error('Unexpected auth type: ' + auth);
   }
 
   var promiseFunction = function (resolve, reject) {
-    // var apiRequest;
-
-    // function success(data) {
-    //   if (resolve) {
-    //     resolve(data);
-    //   }
-    // }
-
-    // function failure(error) {
-    //   if (reject) {
-    //     reject(error);
-    //   }
-    // }
-
-    // function responseHandler(error, response) {
-    //   var data;
-    //   if (error) {
-    //     failure(buildCustomError(error, response));
-    //   } else {
-    //     // In the browser, the file is passed as a blob and in node the file is
-    //     // passed as a string of binary data.
-    //     data = JSON.parse(response.headers['dropbox-api-result']);
-    //     if (response.xhr) {
-    //       data.fileBlob = response.xhr.response;
-    //     } else {
-    //       data.fileBinary = response.res.text;
-    //     }
-    //     success(data);
-    //   }
-    // }
-
     var fetchOptions = {
       method: 'POST',
       headers: {
@@ -68,15 +27,6 @@ downloadRequest = function (path, args, auth, host, accessToken, selectUser) {
         'Dropbox-API-Arg': httpHeaderSafeJson(args)
       }
     };
-
-    // apiRequest = request.post(getBaseURL(host) + path)
-    //   .set('Authorization', 'Bearer ' + accessToken)
-    //   .set('Dropbox-API-Arg', httpHeaderSafeJson(args))
-    //   .on('request', function () {
-    //     if (this.xhr) {
-    //       this.xhr.responseType = 'blob';
-    //     }
-    //   });
 
     if (selectUser) {
       fetchOptions.headers['Dropbox-API-Select-User'] = selectUser;
@@ -107,16 +57,6 @@ downloadRequest = function (path, args, auth, host, accessToken, selectUser) {
       .catch(function (error) {
         reject(buildCustomError(error));
       });
-
-    // Apply the node binary parser to the response if executing in node
-    // if (typeof window === 'undefined') {
-    //   apiRequest
-    //     .buffer(true)
-    //     .parse(nodeBinaryParser)
-    //     .end(responseHandler);
-    // } else {
-    //   apiRequest.end(responseHandler);
-    // }
   };
 
   return new Promise(promiseFunction);
