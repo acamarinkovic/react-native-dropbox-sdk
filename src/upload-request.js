a1e1f6bec20137f3da0cb0aa4e1bcbcf32378aef
Perry Mitchell <perry@perrymitchell.net>
2017-09-19 21:43:25 +0300

    Replace superagent in upload

--- src/upload-request.js
+++ src/upload-request.js
@@ -1,68 +1,51 @@
-var request = require('superagent');
 var Promise = require('es6-promise').Promise;
 var getBaseURL = require('./get-base-url');
+var fetch = require('./fetch');
 var httpHeaderSafeJson = require('./http-header-safe-json');
 
-require('superagent-proxy')(request);
-
 // This doesn't match what was spec'd in paper doc yet
-var buildCustomError = function (error, response) {
+var buildCustomError = function (error) {
   return {
     status: error.status,
-    error: (response ? response.text : null) || error.toString(),
-    response: response
+    error: error.toString()
   };
 };
 
-var uploadRequest = function (path, args, auth, host, accessToken, selectUser, proxy) {
+var uploadRequest = function (path, args, auth, host, accessToken, selectUser) {
   if (auth !== 'user') {
     throw new Error('Unexpected auth type: ' + auth);
   }
 
   var promiseFunction = function (resolve, reject) {
-    var apiRequest;
-
     // Since args.contents is sent as the body of the request and not added to
     // the url, it needs to be remove it from args.
     var contents = args.contents;
     delete args.contents;
 
-    function success(data) {
-      if (resolve) {
-        resolve(data);
-      }
-    }
-
-    function failure(error) {
-      if (reject) {
-        reject(error);
-      }
-    }
-
-    function responseHandler(error, response) {
-      if (error) {
-        failure(buildCustomError(error, response));
-      } else {
-        success(response.body);
-      }
-    }
-
-    apiRequest = request.post(getBaseURL(host) + path)
-      .type('application/octet-stream')
-      .set('Authorization', 'Bearer ' + accessToken)
-      .set('Dropbox-API-Arg', httpHeaderSafeJson(args));
+    var fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        Authorization: 'Bearer ' + accessToken,
+        'Dropbox-API-Arg': httpHeaderSafeJson(args)
+      },
+      body: contents
+    };
 
     if (selectUser) {
-      apiRequest = apiRequest.set('Dropbox-API-Select-User', selectUser);
+      fetchOptions.headers['Dropbox-API-Select-User'] = selectUser;
     }
 
-    if (proxy) {
-      apiRequest.proxy(proxy);
-    }
-
-    apiRequest
-      .send(contents)
-      .end(responseHandler);
+    fetch(getBaseURL(host) + path, fetchOptions)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (res) {
+        resolve(res.body);
+      })
+      .catch(function (error) {
+        reject(buildCustomError(error));
+      });
   };
 
   return new Promise(promiseFunction);
--- test/upload-request.js
+++ test/upload-request.js
@@ -1,39 +1,41 @@
 /* eslint-env mocha */
 var Promise = require('es6-promise').Promise;
 var chai = require('chai');
-var request = require('superagent');
+// var request = require('superagent');
+var fetch = require('../src/fetch');
 var uploadRequest = require('../src/upload-request');
 var sinon = require('sinon');
 
 var assert = chai.assert;
 
 describe('uploadRequest', function () {
-  var stubRequest;
-  var postStub;
-  var endStub;
-  var setStub;
-  var typeStub;
-  var sendStub;
-  var proxyStub;
+  // var stubRequest;
+  var fetchStub;
+  // var endStub;
+  // var setStub;
+  // var typeStub;
+  // var sendStub;
+  // var proxyStub;
 
   beforeEach(function () {
-    stubRequest = {
-      end: function () {},
-      send: function () {},
-      set: function () {},
-      type: function () {},
-      proxy: function () {}
-    };
-    postStub = sinon.stub(request, 'post').returns(stubRequest);
-    endStub = sinon.stub(stubRequest, 'end').returns(stubRequest);
-    setStub = sinon.stub(stubRequest, 'set').returns(stubRequest);
-    typeStub = sinon.stub(stubRequest, 'type').returns(stubRequest);
-    sendStub = sinon.stub(stubRequest, 'send').returns(stubRequest);
-    proxyStub = sinon.stub(stubRequest, 'proxy').returns(stubRequest);
+    // stubRequest = {
+    //   end: function () {},
+    //   send: function () {},
+    //   set: function () {},
+    //   type: function () {},
+    //   proxy: function () {}
+    // };
+    // fetchStub = sinon.stub(request, 'post').returns(stubRequest);
+    // endStub = sinon.stub(stubRequest, 'end').returns(stubRequest);
+    // setStub = sinon.stub(stubRequest, 'set').returns(stubRequest);
+    // typeStub = sinon.stub(stubRequest, 'type').returns(stubRequest);
+    // sendStub = sinon.stub(stubRequest, 'send').returns(stubRequest);
+    // proxyStub = sinon.stub(stubRequest, 'proxy').returns(stubRequest);
+    fetchStub = sinon.spy(fetch.interface, 'fetch');
   });
 
   afterEach(function () {
-    postStub.restore();
+    fetchStub.restore();
   });
 
   it('returns a promise', function () {
@@ -45,69 +47,51 @@ describe('uploadRequest', function () {
 
   it('posts to the correct url', function () {
     uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken');
-    assert(postStub.calledOnce);
-    assert.equal('https://content.dropboxapi.com/2/files/upload', postStub.firstCall.args[0]);
+    assert(fetchStub.calledOnce);
+    assert.equal('https://content.dropboxapi.com/2/files/upload', fetchStub.firstCall.args[0]);
   });
 
   it('sets the request type to application/octet-stream', function () {
     uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken');
-    assert(typeStub.calledOnce);
-    assert.equal('application/octet-stream', typeStub.firstCall.args[0]);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.headers['Content-Type'], 'application/octet-stream');
   });
-
 
   it('sets the authorization header', function () {
     uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken');
-    assert(setStub.calledTwice);
-    assert.equal('Authorization', setStub.firstCall.args[0]);
-    assert.equal('Bearer atoken', setStub.firstCall.args[1]);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.headers.Authorization, 'Bearer atoken');
   });
 
   it('sets the authorization and select user headers if selectUser set', function () {
     uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken', 'selectedUserId');
-    assert(setStub.calledThrice);
-    assert.equal('Authorization', setStub.firstCall.args[0]);
-    assert.equal('Bearer atoken', setStub.firstCall.args[1]);
-    assert.equal('Dropbox-API-Select-User', setStub.thirdCall.args[0]);
-    assert.equal('selectedUserId', setStub.thirdCall.args[1]);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.headers.Authorization, 'Bearer atoken');
+    assert.equal(options.headers['Dropbox-API-Select-User'], 'selectedUserId');
   });
 
   it('sets the Dropbox-API-Arg header', function () {
     uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken');
-    assert(setStub.calledTwice);
-    assert.equal('Dropbox-API-Arg', setStub.secondCall.args[0]);
-    assert.equal(JSON.stringify({ foo: 'bar' }), setStub.secondCall.args[1]);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.headers['Dropbox-API-Arg'], JSON.stringify({ foo: 'bar' }));
   });
 
   it('escapes special characters in the Dropbox-API-Arg header', function () {
     uploadRequest('files/upload', { foo: 'bar单bazá' }, 'user', 'content', 'atoken');
-    assert(setStub.calledTwice);
-    assert.equal('Dropbox-API-Arg', setStub.secondCall.args[0]);
-    assert.equal('{"foo":"bar\\u5355baz\\u00e1"}', setStub.secondCall.args[1]);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.headers['Dropbox-API-Arg'], '{"foo":"bar\\u5355baz\\u00e1"}');
   });
 
   it('doesn\'t include args.contents in the Dropbox-API-Arg header', function () {
     uploadRequest('files/upload', { foo: 'bar', contents: 'fakecontents' }, 'user', 'content', 'atoken');
-    assert(setStub.calledTwice);
-    assert.equal('Dropbox-API-Arg', setStub.secondCall.args[0]);
-    assert.equal(JSON.stringify({ foo: 'bar' }), setStub.secondCall.args[1]);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.headers['Dropbox-API-Arg'], JSON.stringify({ foo: 'bar' }));
   });
 
   it('sends the contents arg as the body of the request', function () {
     uploadRequest('files/upload', { foo: 'bar', contents: 'fakecontents' }, 'user', 'content', 'atoken');
-    assert(sendStub.calledOnce);
-    assert.equal('fakecontents', sendStub.firstCall.args[0]);
-    // assert.equal(JSON.stringify({ foo: 'bar' }), setStub.secondCall.args[1]);
-  });
-
-  it('sets the response handler function', function () {
-    uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken');
-    assert(endStub.calledOnce);
-    assert.isFunction(endStub.firstCall.args[0]);
-  });
-
-  it('sets a proxy', function () {
-    uploadRequest('files/upload', { foo: 'bar' }, 'user', 'content', 'atoken', 'selectedUserId', 'proxy');
-    assert(proxyStub.calledOnce);
+    var options = fetchStub.firstCall.args[1];
+    assert.equal(options.body, 'fakecontents');
+    assert.equal(options.headers['Dropbox-API-Arg'], JSON.stringify({ foo: 'bar' }));
   });
 });
